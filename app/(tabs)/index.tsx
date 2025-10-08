294dd3a1d0090b899e5359b709291176f5762877
Md Hilwan <wan.wanted.1@gmail.com>
2025-10-07 21:52:16 -0500

    remove all the sqlite stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,6 @@
-import * as SQLite from "expo-sqlite";
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import initDatabase, { loadDatabase } from "@/app/db/database";
-import seedSampleData from "@/app/db/seedData";
-import AppNavigator from "@/app/(tabs)/AppNavigator";
+import React from "react";
+import AppNavigator from "@/app/screens/AppNavigator";
 
 export default function App() {
-  const [db, setDb] = useState<SQLite.WebSQLDatabase | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    (async () => {
-      const dbRes = await initDatabase()
-      await seedSampleData(dbRes)
-      const db = await loadDatabase()
-      setDb(db)
-      setLoading(false)
-    })();
-  }, []);
-
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <Text>Loading content...</Text>
-      </View>
-    );
-  }
-
-  return <AppNavigator db={db} />
+  return <AppNavigator />
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#fff", padding: 20 },
-});
