eb57e986cd1eb2d6f9361bcd3ec47eeb6c873ddc
Md Hilwan <wan.wanted.1@gmail.com>
2025-10-07 21:51:36 -0500

    move AppNavigator out from the router

--- app/screens/AppNavigator.tsx
+++ app/screens/AppNavigator.tsx
@@ -3,16 +3,15 @@ import DuaListScreen from "@/app/screens/DuaListScreen";
 import DuaDetailScreen from "@/app/screens/DuaDetailScreen";
 import MapScreen from "@/app/screens/MapScreen";
 import React, {useState} from "react";
-import * as SQLite from "expo-sqlite";
 
-export default function AppNavigator({db}: {db: SQLite.WebSQLDatabase}) {
+export default function AppNavigator() {
   const [duas, setDuas] = useState<any[]>([]);
   const [selectedDua, setSelectedDua] = useState<any>(null);
   const [screen, setScreen] = useState("home");
 
   switch (screen) {
     case "home":
-      return <HomeScreen setScreen={setScreen} db={db} setDuas={setDuas} />
+      return <HomeScreen setScreen={setScreen} setDuas={setDuas} />
     case "duaList":
       return <DuaListScreen setScreen={setScreen} duas={duas} setSelectedDua={setSelectedDua} />
     case "duaDetail":
