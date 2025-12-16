a6fa45c883001a109a585d83302dca680c033f07
Md Hilwan <wan.wanted.1@gmail.com>
2025-12-16 17:03:25 +0800

    fix issue with audio not playing in silent mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {Stack} from 'expo-router';
+import {StatusBar} from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {useColorScheme} from '@/hooks/use-color-scheme';
+import {useEffect} from 'react';
+import {Audio, InterruptionModeAndroid, InterruptionModeIOS} from 'expo-av';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +13,18 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      staysActiveInBackground: false,
+      interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+      playThroughEarpieceAndroid: false,
+    });
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
