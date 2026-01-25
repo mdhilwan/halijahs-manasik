cfede83251acaefc21bf5f38f60dffa9a83e8b16
Md Hilwan <wan.wanted.1@gmail.com>
2026-01-24 23:43:29 -0600

    tokenize contents of about.tsx

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -8,12 +8,13 @@ import {Link} from "expo-router";
 import {Image} from "expo-image";
 import {Collapsible} from "@/components/ui/collapsible";
 import {Ionicons} from "@expo/vector-icons";
+import AboutContent from "@/app/contents/about.json"
 
 const Contributor = ({name, description}: { name: ReactNode, description: ReactNode }) => {
-  return <View style={{marginVertical:10}}>
-    <ThemedText type={"serif"} style={{
+  return <View style={{marginVertical: 10}}>
+    {name && <ThemedText type={"serif"} style={{
       fontSize: 18,
-    }}>{name}</ThemedText>
+    }}>{name}</ThemedText>}
     <ThemedText style={{
       color: "#727272",
     }}>{description}</ThemedText>
@@ -21,11 +22,11 @@ const Contributor = ({name, description}: { name: ReactNode, description: ReactN
 }
 
 const SocialLink = memo(
-  ({ href, icon }: { href: any; icon: ReactNode }) => {
+  ({href, icon}: { href: any; icon: ReactNode }) => {
     return (
-      <Link href={href} style={{ alignSelf: 'flex-start' }}>
+      <Link href={href} style={{alignSelf: 'flex-start'}}>
         <ThemedView style={styles.ctaBtn}>
-          <ThemedText type={'defaultBold'} style={{ color: '#FFFFFF' }}>
+          <ThemedText type={'defaultBold'} style={{color: '#FFFFFF'}}>
             {icon}
           </ThemedText>
         </ThemedView>
@@ -56,60 +57,37 @@ export default function About() {
                 About
             </ThemedText>
         </ThemedView>
+
         <Collapsible title="About the App">
-            <ThemedText type="default" style={{marginVertical: 10}}>
-                Halijah Manasik is a digital Umrah and Hajj guide developed based on the guidebook Menyahut Seruan Ilahi. It is designed to accompany pilgrims through the main rituals of Umrah, whether they are following a mutawwif or performing Umrah on their own.
+          {AboutContent.about.map((text, index) =>
+            <ThemedText key={index} type="default" style={{marginVertical: 10}}>
+              {text}
             </ThemedText>
-            <ThemedText type="default" style={{marginVertical: 10}}>
-                With Allah’s permission, we hope this app will serve as a source of ease and guidance in fulfilling the rites of Hajj and Umrah. May Allah envelop every pilgrim with His Mercy and Pleasure, and bless all who strive and serve along this noble and sacred journey.
-            </ThemedText>
-            <ThemedText type="default" style={{marginVertical: 10}}>
-                May this app also be a lasting source of reward and ongoing charity for everyone who has contributed their efforts to it in any way, and especially for the late founder, Allahyarhamah Halijah Binte Abdul Hamid Almunawwarah, the founder of Halijah Travels, whose love for innovation and dedication to finding new ways to support pilgrims continue to inspire this work.
-            </ThemedText>
+          )}
         </Collapsible>
 
         <Collapsible title="Contributors">
+          {AboutContent.contributors.map(({name, description}, index) =>
             <Contributor
-                name={"Ustaz Haji Sawifi Bin Samson"}
-                description={"Author of \"Menyahut Seruan Ilahi\" and \"Sempurnakan Umrah\", which the app’s core content is adapted from."}
+              key={index}
+              name={name}
+              description={description}
             />
-            <Contributor
-                name={"Ustaz Haji Muhammad Syarafuddeen Bin Mazlan"}
-                description={"Structured and organised the app’s content and recorded and produced majority of the in-app audio."}
-            />
-            <Contributor
-                name={"Ustaz Haji Mohammed Iqbal Bin Abdullah"}
-                description={"Provided verification and review to ensure the accuracy and reliability of the app content."}
-            />
-            <Contributor
-                name={"Muhammad Hilwan Bin Mohamed Idrus"}
-                description={"Software Engineer responsible for the development, and technical implementation and maintenance of the application."}
-            />
-            <ThemedText>We also extend our gratitude to Turntable Music for providing the studio used for the application’s audio recordings. </ThemedText>
+          )}
         </Collapsible>
 
-
         <ThemedText style={{marginVertical: 10}}>
-            To checkout more tours by Halijah Travels, please visit our website and our socials here:
+          {AboutContent.footer.socialsIntro}
         </ThemedText>
 
-        <View style={{ flexDirection: 'row', gap: 20 }}>
-          <SocialLink
-            href="https://halijah.com.sg/group-tours"
-            icon={<Ionicons name={"globe"} size={25} />}
-          />
-          <SocialLink
-            href="https://www.instagram.com/halijahtravels"
-            icon={<Ionicons name={"logo-instagram"} size={25} />}
-          />
-          <SocialLink
-            href="https://www.facebook.com/HalijahTravels"
-            icon={<Ionicons name={"logo-facebook"} size={25} />}
-          />
-          <SocialLink
-            href="https://www.youtube.com/@HalijahTravels"
-            icon={<Ionicons name={"logo-youtube"} size={25} />}
-          />
+        <View style={{flexDirection: 'row', gap: 20}}>
+          {AboutContent.footer.socialLinks.map(({href, icon}, index) =>
+            <SocialLink
+              key={index}
+              href={href}
+              icon={<Ionicons name={icon as any} size={25} />}
+            />
+          )}
         </View>
     </ParallaxScrollView>
   );
@@ -140,7 +118,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 30,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.15,
     shadowRadius: 4,
     elevation: 3,
