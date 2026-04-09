3a88865e082de81eecff5aba1b9ef7f3b1f9a1e8
Md Hilwan <wan.wanted.1@gmail.com>
2026-04-09 16:35:58 +0800

    remove comments

--- cms/app/component/Sidebar.tsx
+++ cms/app/component/Sidebar.tsx
@@ -255,16 +255,11 @@ function UmrahBuilderIcon({ className }: { className?: string }) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      {/* Kaaba representation */}
       <rect x="7" y="8" width="10" height="10" rx="1" />
-      {/* Door/entrance */}
       <rect x="10" y="13" width="4" height="5" />
-      {/* Kiswa band */}
       <line x1="7" y1="11" x2="17" y2="11" />
-      {/* Building/planning elements - blocks representing itinerary */}
       <rect x="3" y="3" width="4" height="3" rx="0.5" />
       <rect x="17" y="3" width="4" height="3" rx="0.5" />
-      {/* Connecting lines representing journey/flow */}
       <path d="M5 6v2h2" />
       <path d="M19 6v2h-2" />
     </svg>
