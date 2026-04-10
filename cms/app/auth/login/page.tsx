be8cb9e84c9545119f3087ed17cce58b7c6f37fd
Md Hilwan <wan.wanted.1@gmail.com>
2026-04-11 01:27:41 +0800

    remove redirectTo param

--- cms/app/auth/login/page.tsx
+++ cms/app/auth/login/page.tsx
@@ -14,12 +14,7 @@ export default function LoginPage() {
     try {
       const supabase = createClient()
       const { error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-          redirectTo:
-            process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL ??
-            `${window.location.origin}/auth/callback`,
-        },
+        provider: 'google'
       })
 
       if (error) throw error
