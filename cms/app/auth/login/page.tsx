c973d18de64f5033cd114fcbaffb112d4e1dd2c7
Md Hilwan <wan.wanted.1@gmail.com>
2026-04-11 01:36:16 +0800

    put back the  redirectTo param

--- cms/app/auth/login/page.tsx
+++ cms/app/auth/login/page.tsx
@@ -14,7 +14,10 @@ export default function LoginPage() {
     try {
       const supabase = createClient()
       const { error } = await supabase.auth.signInWithOAuth({
-        provider: 'google'
+        provider: 'google',
+        options: {
+          redirectTo: `${window.location.origin}/auth/callback`,
+        },
       })
 
       if (error) throw error
