18272782f8f33c0fcffee01f5906bc2cfba9aac7
Md Hilwan <wan.wanted.1@gmail.com>
2026-04-16 23:48:16 -0500

    replace localhost:8080 since on digitalocean origin will always be localhost:8080

--- cms/app/auth/callback/route.ts
+++ cms/app/auth/callback/route.ts
@@ -18,14 +18,19 @@ export async function GET(request: NextRequest) {
       const supabase = await createClient()
       console.log('[CALLBACK] Exchanging code for session...')
       const { error } = await supabase.auth.exchangeCodeForSession(code)
-      
+
       if (error) {
         console.log('[CALLBACK] Error exchanging code:', error)
         return NextResponse.redirect(`${origin}/auth/error`)
       }
 
+      let redirectTo = `${origin}${next}`
+      if (redirectTo.indexOf('https://localhost:8080') > -1) {
+        console.log('[CALLBACK] Replacing localhost in redirect URL for development environment')
+        redirectTo.replace('https://localhost:8080', process.env.NEXT_PUBLIC_LOCALHOST_REPLACE ?? 'localhost')
+      }
       console.log('[CALLBACK] Success! Redirecting to:', `${origin}${next}`)
-      return NextResponse.redirect(`${origin}${next}`)
+      return NextResponse.redirect(`${redirectTo}`)
     } catch (err) {
       console.error('[CALLBACK] Exception caught:', err)
       return NextResponse.redirect(`${origin}/auth/error`)
